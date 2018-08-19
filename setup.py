a352cc0d092d8996080a27d4c2595cb2aab63d15
devmessias <messias.physics@gmail.com>
2018-08-19 00:15:33 -0300

    Fixes #3

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 from setuptools import setup, find_packages
 
-readme = open('README','r')
+readme = open('README', 'r')
 README_TEXT = readme.read()
 readme.close()
 
 setup(
     name="tkuramoto",
     version="0.0.1",
-    packages = find_packages(exclude=["build",]),
-    scripts=["tkuramoto/bin/tkuramoto"],
-    long_description = README_TEXT,
-    install_requires=["tensorflow", "scipy", "numpy"],
+    packages=find_packages(exclude=["build", ]),
+    long_description=README_TEXT,
+    # install_requires=["tensorflow-gpu", "scipy", "numpy"],
     include_package_data=True,
     license="AGPLv3",
     description="Dynamics in large networks",
-    author="Bruno Messias",
     author_email="messias.physics@gmail.com",
-    download_url="https://github.com/devmessias/phd_tkuramoto/archive/0.0.1.tar.gz",
-    keywords=["gpu", "science","complex-networks", "dynamics"],
-
+    author="Bruno Messias; Thomas K. Peron",
+    # download_url=
+    # "https://github.com/devmessias/phd_tkuramoto/archive/0.0.1.tar.gz",
+    keywords=[
+        "gpu", "science", "complex-networks", "graphs", "dynamics",
+         "tensorflow"
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        ("License :: OSI Approved :: GNU Affero General Public License v3 or",
+         "later  (AGPLv3+)"),
         "Intended Audience :: Science/Research",
         "Programming Language :: Python",
         "Topic :: Text Processing :: Markup :: LaTeX",
